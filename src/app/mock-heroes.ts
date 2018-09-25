74f29597e3fc5ea0646e66d18edf414597b2dfed
denvernash <35231540+denvernash@users.noreply.github.com>
2018-09-25 14:48:41 -0400

    finished extra credit

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Mr. Nice', pic: 'https://images.pexels.com/photos/346796/pexels-photo-346796.jpeg?auto=compress&cs=tinysrgb&h=350' },
+  { id: 12, name: 'Narco', pic: 'https://images.pexels.com/photos/38630/bodybuilder-weight-training-stress-38630.jpeg?auto=compress&cs=tinysrgb&h=350' },
+  { id: 13, name: 'Bombasto', pic: 'https://images.pexels.com/photos/69934/firefighters-fire-flames-outside-69934.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'},
+  { id: 14, name: 'Celeritas', pic: 'https://cdn.pixabay.com/photo/2016/06/05/07/10/photographer-1436917_960_720.jpg' },
+  { id: 15, name: 'Magneta', pic: 'https://cdn.pixabay.com/photo/2014/04/02/10/50/superhero-304713__340.png'},
+  { id: 16, name: 'RubberMan', pic: 'https://images.pexels.com/photos/4048/strength-strong-toy-action-figure.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+  { id: 17, name: 'Dynama', pic: 'https://cdn.pixabay.com/photo/2016/08/17/22/22/kapow-1601675__340.png' },
+  { id: 18, name: 'Dr IQ', pic: 'https://cdn.pixabay.com/photo/2016/07/18/23/43/mr-1527208__340.jpg' },
+  { id: 19, name: 'Magma', pic: 'https://cdn.pixabay.com/photo/2013/07/13/11/56/superhero-158999__340.png' },
+  { id: 20, name: 'Tornado', pic: 'https://cdn.pixabay.com/photo/2016/05/02/17/40/batman-1367730__340.jpg' }
 ];
